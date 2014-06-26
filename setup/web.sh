73290c33bfde580b85f73472b8885da9ec38822f
Martin van Beurden <chadoe@gmail.com>
2014-06-26 19:13:14 +0200

    Remove default webpage.

--- setup/web.sh
+++ setup/web.sh
@@ -5,8 +5,6 @@ source setup/functions.sh # load our functions
 
 apt_install nginx php5-fpm
 
-rm -f /etc/nginx/sites-enabled/default
-
 STORAGE_ROOT_ESC=$(echo $STORAGE_ROOT|sed 's/[\\\/&]/\\&/g')
 PUBLIC_HOSTNAME_ESC=$(echo $PUBLIC_HOSTNAME|sed 's/[\\\/&]/\\&/g')
 
@@ -15,12 +13,7 @@ PUBLIC_HOSTNAME_ESC=$(echo $PUBLIC_HOSTNAME|sed 's/[\\\/&]/\\&/g')
 cat conf/nginx.conf \
 	| sed "s/\$STORAGE_ROOT/$STORAGE_ROOT_ESC/g" \
 	| sed "s/\$PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME_ESC/g" \
-	> /etc/nginx/conf.d/local.conf
-
-# make a default homepage
-mkdir -p $STORAGE_ROOT/www/static
-cp conf/www_default.html $STORAGE_ROOT/www/static/index.html
-chown -R $STORAGE_USER $STORAGE_ROOT/www/static/index.html
+	> /etc/nginx/sites-available/default
 
 #fpm settings
 sed -i "s/^;listen.owner = www-data/listen.owner = www-data/" /etc/php5/fpm/pool.d/www.conf
