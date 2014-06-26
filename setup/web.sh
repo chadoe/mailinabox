c51dbf3a6c004d92f66d5d6504cf2e28a2b34dde
Martin van Beurden <chadoe@gmail.com>
2014-06-26 21:00:00 +0200

    nginx config

--- setup/web.sh
+++ setup/web.sh
@@ -11,13 +11,15 @@ apt_install nginx php5-fpm
 STORAGE_ROOT_ESC=$(echo $STORAGE_ROOT|sed 's/[\\\/&]/\\&/g')
 PUBLIC_HOSTNAME_ESC=$(echo $PUBLIC_HOSTNAME|sed 's/[\\\/&]/\\&/g')
 
+rm /etc/nginx/sites-enabled/default
+
 # copy in the nginx configuration file and substitute some
 # variables
 cat conf/nginx.conf \
 	| sed "s/\$STORAGE_ROOT/$STORAGE_ROOT_ESC/g" \
 	| sed "s/\$PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME_ESC/g" \
-	> /etc/nginx/sites-available/webmail
-ln -s /etc/nginx//sites-available/roundcube /etc/nginx//sites-enabled/webmail
+	> /etc/nginx/sites-available/webmail.conf
+ln -s /etc/nginx//sites-available/roundcube /etc/nginx//sites-enabled/
 
 #fpm settings
 sed -i "s/^;listen.owner = www-data/listen.owner = www-data/" /etc/php5/fpm/pool.d/www.conf
